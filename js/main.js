e2e8c1eb8686d2b7889b0fa30546669de7678041
James <100710683+greedywind@users.noreply.github.com>
2023-03-09 22:40:23 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,21 @@
+// Sorry, this code is messy.
 
+$('#submit').on('click', function () {
+	var length = $('#length').val();
+	var width = $('#width').val();
+	var depth = $('#depth').val();
+
+	$('#manure').html((((length * width * depth) * 0.00422675) * .6) * .35);
+	$('#fiber').html((((length * width * depth) * 0.00422675) * .6) * .29);
+	$('#vermiculite').html((((length * width * depth) * 0.00422675) * .6) * .30);
+	$('#coffee').html((((length * width * depth) * 0.00422675) * .6) * .05);
+	$('#gypsum').html(((((length * width * depth) * 0.00422675) * .6) * .01) * 16 );
+	$('#water').html((((((length * width * depth) * 0.00422675) * .6) * .24) * .4) * 4.16667);
+
+	$('#dry').html((((length * width * depth) * 0.00422675) * .4) * .7);
+	$('#gypsum1').html((((((length * width * depth) * 0.00422675) * .4) * .7) * 0.253605) / 3);
+	$('#brewed').html((((((length * width * depth) * 0.00422675) * .4) * .7) * 0.253605) / 1.5);
+
+	$('P').addClass('black');
+
+});
